1612e53cececb1524f179a688aa721b91a8c31a9
Adam Walker <awalker@virtualmindset.com>
2014-06-19 17:35:27 -0400

    Ignore an irksome warning in another lib

--- src/Base/Error.php
+++ src/Base/Error.php
@@ -23,6 +23,7 @@ class Error {
     // Ignore errors less than the current error setting
     if((error_reporting() & $code) === 0) return TRUE;
     if(strpos($error, 'magic_quotes_gpc') !== false) return true;
+    if($code == E_WARNING && strpos($file, '/vendor/') !== false) return true;
     $user = IS_COMMAND ? 'COMMAND' : 'Not a command';
     $view = new View('system/error');
     $view->error = $error;
