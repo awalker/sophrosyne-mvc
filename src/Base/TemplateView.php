df9270faebaaf6eff899f90e2f9f0c8dcabea0f8
Adam Walker <awalker@virtualmindset.com>
2014-07-14 15:16:11 -0400

    Restore the old View class and move the new one to TemplateView

---
+++ src/Base/TemplateView.php
@@ -0,0 +1,185 @@
+<?php
+namespace Base;
+$IN_VIEW = null;
+class TemplateView extends View {
+  private $__view = NULL;
+
+  public $viewEnableFiltering = true;
+
+  /**
+   * Returns a new view object for the given view.
+   *
+   * @param string $file the view file to load
+   * @param string $module name (blank for current theme)
+   */
+  public function __construct($file, $values = null)
+  {
+    $this->__view = $file;
+    if(!is_null($values)) {
+      $this->set((array)$values);
+    }
+  }
+
+
+  /**
+   * Set an array of values
+   *
+   * @param array $array of values
+   */
+  public function set($array)
+  {
+    foreach($array as $k => $v)
+    {
+      if(strpos($k, "\\") === false) {
+        $this->$k = $v;
+      }
+    }
+    return $this;
+  }
+
+  public function getViewFilename() {
+    return static::$directory . $this->__view . static::$ext;
+  }
+
+
+  /**
+   * Return the view's HTML
+   *
+   * @return string
+   */
+  public function __toString()
+  {
+    try {
+      return $this->render();
+    }
+    catch(\Exception $e)
+    {
+      Error::exception($e);
+      return '';
+    }
+  }
+
+  public function process($clean) {
+    $pattern = '/\{\{(.+?)\}\}/';
+    return preg_replace_callback($pattern, array($this, 'processSingle'), $clean);
+  }
+
+  public function processSingle($matches) {
+    if (!$this->viewEnableFiltering and trim($matches[1]) == 'START FILTERING') {
+      $this->viewEnableFiltering = true;
+      return '';
+    }
+    if (!$this->viewEnableFiltering) {return $matches[0];}
+    if (trim($matches[1]) == 'STOP FILTERING') {
+      $this->viewEnableFiltering = false;
+      return '';
+    }
+    try {
+      $parts = explode(' ', trim($matches[1]));
+      $path = $parts[0];
+      switch($parts[0]) {
+        case 'for':
+          return $this->processFor($matches, $parts); break;
+        case 'partial':
+          return $this->processPartial($matches, $parts); break;
+        case 'ifpartial':
+          return $this->processIfPartial($matches, $parts); break;
+        default:
+          if(count($parts) > 1) {
+            return '<strong style="color: red;">' . h($matches[0]) . '</strong>';
+          }
+      }
+      $processor = 'h';
+      $ps = explode('|', $path);
+      if(count($ps) > 1) {
+        $processor = $ps[0];
+        $path = $ps[1];
+      }
+      $out = getFromContext($this, $path);
+      if (is_object($out) && is_a($out, 'Base\View')) {
+        return (string)$out;
+      }
+      if($processor) {
+        return $processor($out);
+      } else {
+        return $out;
+      }
+    } catch(\Exception $e) {
+      return '<strong style="color: red;">' . h($matches[0]) . '<br>' . h($e->getMessage()) . '</strong>';
+    }
+  }
+
+  public function processIfPartial($matches, $parts) {
+    $value = getFromContext($this, $parts[1]);
+    if ($value) {
+      return $this->processPartial($matches, $parts, 2);
+    }
+    return '';
+  }
+
+  public function processPartial($matches, $parts, $extra = 1) {
+    $view = getFromContext($this, $viewPath = array_pop($parts));
+    if (!$view or !is_object($view)) {
+      throw new \Exception("$viewPath did not resolve to a view");
+    }
+    $that = $this;
+    if(count($parts) > $extra) {
+      $that = getFromContext($this, array_pop($parts));
+    }
+    $view->set((array)$that);
+    if(property_exists($this, 'controller')) {
+      $view->controller = $this->controller;
+    }
+    return (string)$view;
+  }
+
+  public function processFor($matches, $parts) {
+    $collection = getFromContext($this, $parts[1]);
+    $notFoundViewPath = null;
+    if(count($parts) > 4) {
+      $notFoundViewPath = array_pop($parts);
+    }
+    if(count($parts) > 4) { array_pop($parts); }
+    $viewPath = array_pop($parts);
+    $view = getFromContext($this, $viewPath);
+    if (!$view or !is_object($view)) {
+      throw new \Exception("$viewPath did not resolve to a view");
+    }
+    $rendered = array();
+    if ($collection && is_object($view) && is_a($view, 'Base\View')) {
+      foreach($collection as $item) {
+        $view->set((array)$item);
+        if(property_exists($this, 'controller')) {
+          $view->controller = $this->controller;
+        }
+        $rendered[] = (string)$view;
+      }
+    } elseif($notFoundViewPath) {
+      $view = getFromContext($this, $notFoundViewPath);
+      if (!$view or !is_object($view)) {
+        throw new \Exception("$notFoundViewPath did not resolve to a view");
+      }
+      return (string)$view;
+    }
+    return implode('', $rendered);
+  }
+
+  /**
+   * Renders the view without trapping exceptions
+   * @return [type] [description]
+   */
+  public function render()
+  {
+    if ($this->__view != 'system/exception' && $this->__view != 'system/error') {
+      $GLOBALS['IN_VIEW'] = &$this;
+    }
+    ob_start();
+    extract((array) $this);
+    require $this->getViewFilename();
+    $clean = ob_get_clean();
+
+    return $this->process($clean);
+  }
+
+
+}
